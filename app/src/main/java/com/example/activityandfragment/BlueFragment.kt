1ff62f85f9ed21e3570bacc0c7a5544578a2a400
k1m2njun <k1m2njun@naver.com>
2023-11-14 02:20:27 +0900

    Fragment를 이용한 화면 전환

--- app/src/main/java/com/example/activityandfragment/BlueFragment.kt
+++ app/src/main/java/com/example/activityandfragment/BlueFragment.kt
@@ -1,8 +1,18 @@
 package com.example.activityandfragment
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 
 class BlueFragment : Fragment() {
-    // 프래그먼트 레이아웃 작성
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_blue, container, false)
+    }
 
 }
--- app/src/main/java/com/example/activityandfragment/RedFragment.kt
+++ app/src/main/java/com/example/activityandfragment/RedFragment.kt
@@ -1,8 +1,21 @@
 package com.example.activityandfragment
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 
 class RedFragment : Fragment() {
     // 프래그먼트 레이아웃 작성
-
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_red, container, false)
+        // layout
+        // container: 부모 view - 여기에서는 activity_two_color.frame_layout
+        // attachToRoot: fragment 자동 추가 ? false
+    }
 }
